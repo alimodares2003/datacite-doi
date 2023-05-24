b5eb08e44f8bbe25d84066a6c62c426c3901b20e
ali <s.a.modares.h@gmail.com>
2023-05-24 19:49:20 +0330

    initial

---
+++ src/main/java/com/example/datacitedoi/DataciteDoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.datacitedoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataciteDoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataciteDoiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datacitedoi/DataciteDoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datacitedoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataciteDoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
